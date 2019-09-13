991cfa53c5c065dd85305c16e880fe9966145874
pnhung177 <pnhung177@acegik.net>
2019-09-13 13:44:55 +0700

    Export the uniqid & uuid libraries

--- test/tdd/id-generator-test.js
+++ test/tdd/id-generator-test.js
@@ -16,4 +16,24 @@ describe('id-generator', function() {
       assert.lengthOf(idGenerator.getId(), 36);
     });
   });
+
+  describe('multiple generators', function () {
+    it('the generators are independent with each others', function () {
+      var idgen1 = idGenerator.new({ type: 'uniqid' });
+      var idgen2 = idGenerator.new({ type: 'base64' });
+      assert.lengthOf(idGenerator.getId(), 36);
+      assert.isAtMost(idgen1.getId().length, 18);
+      assert.equal(idgen2.getId().length, 22);
+    });
+  });
+
+  describe('static functions, properties', function () {
+    it('export the uniqid & uuid libraries properly', function () {
+      assert.isFunction(idGenerator.new);
+      assert.isFunction(idGenerator.uniqid);
+      assert.isFunction(idGenerator.uuid);
+      assert.isFunction(idGenerator.uuid.v1);
+      assert.isFunction(idGenerator.uuid.v4);
+    })
+  });
 });
